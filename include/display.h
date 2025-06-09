98a3f473f5642c10dee0d81edbc63e1d82a4c93e
JGomezC145 <jgomezcyn@outlook.com>
2025-06-09 15:57:29 -0600

    SomeMiscFeatures

--- include/display.h
+++ include/display.h
@@ -14,4 +14,4 @@ void setStatus(StatusType c, bool ok);
 
 // Tarea que actualiza hora y otros indicadores (se corre en core 1)
 void startDisplayTask();
-void getTemperature()
+void getTemperature();
