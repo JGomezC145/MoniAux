f4be962e96217055fde60448b8800c64c23c9911
JGomezC145 <jgomezcyn@outlook.com>
2025-06-27 17:06:38 -0600

    Añadido manejo de eventos de entrada y verificación de conexión WiFi en el bucle principal.
    Added InputEvents and WiFi verification for OTA

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "wifi_ota.h"
 #include "display.h"
 #include "serial_comm.h"
+#include "input.h"
 
 void setup(){
     pinMode(RGB_PIN, OUTPUT);
@@ -14,9 +15,41 @@ void setup(){
     
     serial_setup();   // handshake con MCU interno
     
-    //startDisplayTask();
+    startDisplayTask();
+    startInputTask();
 }
 
 void loop(){ //core 0
+
+    //check if WiFi is connected
+    if(!connectedWifi){
+        ArduinoOTA.handle(); 
+    }
+
+    InputEvent ev;
+    if(getInputEvent(ev,0)){     // no bloquea
+        switch(ev.type){
+            case KEYPAD_CHAR:
+                Serial.printf("Btn%c\n", ev.data);
+                if (ev.data == '1') {
+                    textoCentrado("TestForSC", 80, ST77XX_RED, 1);
+                } else if (ev.data == 'C') {
+                    textoCentrado("          ", 80, ST77XX_RED, 1);
+                }
+                break;
+
+            case ENC_ROT_LEFT:
+                Serial.println("EncoderL");
+                break;
+
+            case ENC_ROT_RIGHT:
+                Serial.println("EncoderR");
+                break;
+
+            case ENC_BTN_PRESS:
+                Serial.println("EncoderSW");
+                break;
+        }
+    }
     serial_loop();
 }
